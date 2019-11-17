c43edc9e19905802f6e1997669f4f26029f24a20
coco520 <feiyu@tg10010.com>
2019-11-17 15:12:07 +0700

    '闭包'

--- basicGo/basic/lesson/inter.go
+++ basicGo/basic/lesson/inter.go
@@ -2,6 +2,10 @@ package lesson
 
 import "fmt"
 
+/*
+interface 接口
+*/
+
 type person01 struct {
 	name string
 }
@@ -64,8 +68,43 @@ func (p person01) fly() {
 
 
 func Face(){
-	joy := person01{name:"fangzi"}
-	show(joy)
-	play(joy)
-	fly(joy)
+	//joy := person01{name:"fangzi"}
+	//show(joy)
+	//play(joy)
+	//fly(joy)
+
+	play := eneny()
+	play()
+	play()
+	play()
+
+	adder(2,3)
+
+	fmt.Printf("%s\n","----")
+
+	sum := curryAdd(1)
+	sum(2)
+	sum(3)
+}
+
+
+/*
+闭包
+*/
+func eneny() func() {
+	key := 0
+	return func() {
+		key++
+		fmt.Printf("%d\n",key)
+	}
+}
+
+func adder(a,b int){
+	fmt.Println(a+b)
+}
+
+func curryAdd(a int) func(int){
+	return func(b int) {
+		fmt.Printf("%v\n",a + b)
+	}
 }
