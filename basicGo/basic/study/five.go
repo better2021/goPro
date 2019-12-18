eff4eecbd407fcf4ca6256996484d56a0ea75f1b
coco520 <feiyu@tg10010.com>
2019-12-18 18:47:40 +0700

    'auti'

--- basicGo/basic/study/five.go
+++ basicGo/basic/study/five.go
@@ -15,7 +15,23 @@ func Five(){
 	//}
 	// time.Sleep(time.Millisecond)
 
-	chanDemo()
+	//chanDemo()
+
+	/*
+	iota 是go的关键字出现时将被重置为0，const中每新增一行常量声明将使iota计数一次
+	iota 实行枚举类型
+	*/
+
+	const (
+		n1 = iota // 0
+		n2 = 10
+		n3 = iota
+		n4
+		n5
+	)
+
+	fmt.Println(n2,n4,n5)
+	fmt.Println(n1+n3)
 }
 
 func chanDemo(){
