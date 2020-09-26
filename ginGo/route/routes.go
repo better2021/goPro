c142b85e4f3b7d3f017c06d5695aec2422dbed92
coco520 <feiyu@tg10010.com>
2020-09-26 17:03:50 +0700

    'time'

--- ginGo/route/routes.go
+++ ginGo/route/routes.go
@@ -3,11 +3,19 @@ package route
 import (
 	"ginGo/controller"
 	"ginGo/middleware"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CollectRouter(r *gin.Engine) *gin.Engine {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"time":time.Now().Format("2006-01-02 15:04:05"),
+		})
+	})
+
 	r.Use(middleware.CorsMiddleware(),middleware.RecoveryMiddleware()) // 使用跨域中间件 和 cover()中间件
 	r.POST("/api/auth/register", controller.Register)
 	r.POST("/api/auth/login",controller.Login)
